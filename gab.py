1ebc1ef6c9f3acebdae2fd9bede8ee7c739c58b0
Gab311 <72075754+Gab311@users.noreply.github.com>
2022-07-10 19:00:42 +0800

    Update gab.py

--- gab.py
+++ gab.py
@@ -1,4 +1,20 @@
 import random
+import requests
+from pyfiglet import figlet_format
+import time
+import sys
 
 def randomized_number():
     return random.randint(0, 10)
+
+
+def gab_program(msg, webhook):
+    print(figlet_format("gab pogi")
+    while True:
+        try:
+            data.requests.post(webhook, json={"content": msg})
+        except:
+            print("bad webhook %s" $ (webhook))
+            wait(5)
+            sys.exit()
+            
